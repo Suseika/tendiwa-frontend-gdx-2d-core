a63d88bc2609cb9f12a5e747c55325e126fed299
Georgy Vlasov <wlasowegor@gmail.com>
2016-03-03 02:55:33 +0500

    Add configurable ApplicationListener to HeadlessGdxTest

--- src/main/kotlin/org/tendiwa/client/gdx/testing/HeadlessGdxTest.kt
+++ src/main/kotlin/org/tendiwa/client/gdx/testing/HeadlessGdxTest.kt
@@ -1,6 +1,7 @@
 package org.tendiwa.client.gdx.testing
 
 import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.ApplicationListener
 import com.badlogic.gdx.backends.headless.HeadlessApplication
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration
 import org.junit.After
@@ -9,12 +10,13 @@ import org.junit.After
  * Base test class for testing classes that require LibGDX to be initialized to
  * run properly.
  */
-abstract class HeadlessGdxTest {
+abstract class HeadlessGdxTest(
+    applicationListener: ApplicationListener = object : ApplicationAdapter() {}
+) {
     protected val application =
         HeadlessApplication(
-            object : ApplicationAdapter() {},
-            HeadlessApplicationConfiguration().apply
-            {
+            applicationListener,
+            HeadlessApplicationConfiguration().apply {
                 renderInterval = 1f / 60
             }
         )
