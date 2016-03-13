54bce645284d2d0c1828ddc6de78866354b3fa4d
Georgy Vlasov <wlasowegor@gmail.com>
2016-03-13 15:02:02 +0500

    Document WallActor

--- src/main/kotlin/org/tendiwa/client/gdx/walls/WallActor.kt
+++ src/main/kotlin/org/tendiwa/client/gdx/walls/WallActor.kt
@@ -4,6 +4,13 @@ import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.scenes.scene2d.Actor
 
+/**
+ * Walls are implemented as [Actor]s instead of a single layer similar to
+ * [FloorLayer] because otherwise it would be very difficult to occlude things
+ * behind wall pieces and at the same time occlude wall pieces with things in
+ * front of the wall pieces.
+ */
+// TODO: Reimplement wall drawing using a single actor per horizontal row.
 class WallActor(
     private val region: TextureRegion,
     x: Int,
