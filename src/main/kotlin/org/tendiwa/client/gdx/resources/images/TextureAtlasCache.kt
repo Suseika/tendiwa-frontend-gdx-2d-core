2c513c39f7b247b2227d79040bbf7e0b5ffe848b
Georgy Vlasov <wlasowegor@gmail.com>
2016-03-16 03:40:04 +0500

    Implement a disk cache for TextureAtlases. Fixes #T-27

---
+++ src/main/kotlin/org/tendiwa/client/gdx/resources/images/TextureAtlasCache.kt
@@ -0,0 +1,77 @@
+package org.tendiwa.client.gdx.resources.images
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.tools.texturepacker.TexturePacker
+import java.io.PrintWriter
+import java.nio.file.Files
+import java.nio.file.Path
+
+class TextureAtlasCache(
+    private val directory: Path,
+    private val textureBundle: ClasspathTextureBundle
+) {
+    companion object {
+        private val HASH_FILE_NAME = "hash"
+    }
+
+    fun obtainAtlas(): TextureAtlas {
+        if (directory.toFile().exists()) {
+            if (!directory.toFile().isDirectory) {
+                throw IllegalArgumentException(
+                    "Path for cache directory $directory is not a directory"
+                )
+            }
+            if (!isCacheValid()) {
+                regenerate()
+            }
+        } else {
+            regenerate()
+        }
+        return TextureAtlas(packFile())
+    }
+
+    internal fun isCacheValid(): Boolean {
+        val hashFile = directory.resolve(HASH_FILE_NAME)
+        return if (hashFile.toFile().isDirectory || !hashFile.toFile().exists()) {
+            false
+        } else {
+            storedHash(hashFile) == textureBundle.hash
+        }
+    }
+
+    private fun packFile() =
+        Gdx.files.absolute(
+            directory
+                .resolve("atlas")
+                .resolve("universal.atlas")
+                .toAbsolutePath()
+                .toString()
+        )
+
+    private fun storedHash(hashFile: Path): String =
+        String(
+            Files.readAllBytes(hashFile),
+            Charsets.UTF_8
+        )
+
+
+    fun regenerate() {
+        directory.toFile().mkdir()
+        writeHash(textureBundle.hash)
+        val extractDirectory = directory.resolve("extracted")
+        textureBundle.extractTo(extractDirectory)
+        TexturePacker.process(
+            extractDirectory.toString(),
+            directory.resolve("atlas").toString(),
+            "universal"
+        )
+        extractDirectory.toFile().deleteRecursively()
+    }
+
+
+    private fun writeHash(hash: String) {
+        PrintWriter(directory.resolve(HASH_FILE_NAME).toFile())
+            .use { it.write(hash) }
+    }
+}
