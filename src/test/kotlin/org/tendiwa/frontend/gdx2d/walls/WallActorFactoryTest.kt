c976856809a05ac13bfa8131d81598b8c727e6bd
Georgy Vlasov <wlasowegor@gmail.com>
2016-04-06 20:10:22 +0500

    Implement storage and updating of lighting. For #T-47

--- src/test/kotlin/org/tendiwa/frontend/gdx2d/walls/WallActorFactoryTest.kt
+++ src/test/kotlin/org/tendiwa/frontend/gdx2d/walls/WallActorFactoryTest.kt
@@ -9,6 +9,8 @@ import org.tendiwa.backend.GridParallelepiped
 import org.tendiwa.backend.by
 import org.tendiwa.backend.space.Space
 import org.tendiwa.backend.space.Voxel
+import org.tendiwa.backend.space.lighting.ConstantLuminary
+import org.tendiwa.backend.space.lighting.Luminosity
 import org.tendiwa.frontend.gdx2d.resources.images.NamedTextureCache
 import org.tendiwa.frontend.gdx2d.testing.LwjglGdxRule
 import org.tendiwa.frontend.generic.RenderingVicinity
@@ -37,7 +39,11 @@ class WallActorFactoryTest {
 
     private fun createMockVicinity(): RenderingVicinity =
         RenderingVicinity(
-            Space(GridParallelepiped(Voxel(0, 0, 0), 32 by 32 by 1)),
+            Space(
+                GridParallelepiped(Voxel(0, 0, 0), 32 by 32 by 1),
+                listOf(true),
+                ConstantLuminary(Luminosity.SUNNY)
+            ),
             32 by 32,
             0
         )
